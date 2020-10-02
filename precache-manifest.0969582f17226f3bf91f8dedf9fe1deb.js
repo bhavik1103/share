5aa8d1e055a07c3b450e36d8626dcc54c6db5bf1
Saif-Mulla <saifmulla444@gmail.com>
2020-10-02 09:19:09 +0530

    Updates

---
+++ precache-manifest.0969582f17226f3bf91f8dedf9fe1deb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "098a2dde6ed5533d54324832ffc13935",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "63084d49018a0f276ced",
+    "url": "/Todo-App/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8d21629c41174ceec1c8",
+    "url": "/Todo-App/static/js/2.44b8750f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-App/static/js/2.44b8750f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63084d49018a0f276ced",
+    "url": "/Todo-App/static/js/main.5806265d.chunk.js"
+  },
+  {
+    "revision": "1db120321b35db2c892b",
+    "url": "/Todo-App/static/js/runtime-main.7e0692b0.js"
+  }
+]);
